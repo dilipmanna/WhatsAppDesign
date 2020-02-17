088688fae7690513f3821619bfe91479b2d8a4be
dilipmanna <dilipmanna36@gmail.com>
2020-02-17 20:08:20 +0530

    First Commit

---
+++ app/src/main/java/com/example/whatsapp/activities/AgreeContinueActivity.kt
@@ -0,0 +1,54 @@
+package com.example.whatsapp.activities
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.*
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.example.whatsapp.R
+import kotlinx.android.synthetic.main.activity_agree_continue.*
+
+class AgreeContinueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agree_continue)
+
+
+        tv_privacy_policy.makeLinks(
+            Pair("Terms of Service", View.OnClickListener {
+                Toast.makeText(applicationContext, "Terms of Service Clicked", Toast.LENGTH_SHORT).show()
+            }),
+            Pair("Privacy Policy", View.OnClickListener {
+                Toast.makeText(applicationContext, "Privacy Policy Clicked", Toast.LENGTH_SHORT).show()
+            }))
+        tv_privacy_policy.setHighlightColor(Color.TRANSPARENT);
+    }
+
+    fun TextView.makeLinks(vararg links: Pair<String, View.OnClickListener>) {
+        val spannableString = SpannableString(this.text)
+        for (link in links) {
+            val clickableSpan = object : ClickableSpan() {
+                override fun onClick(view: View) {
+                    Selection.setSelection((view as TextView).text as Spannable, 0)
+                    view.invalidate()
+                    link.second.onClick(view)
+                }
+
+                override fun updateDrawState(ds: TextPaint) {
+                    super.updateDrawState(ds)
+                    ds.setUnderlineText(false)
+                }
+            }
+            val startIndexOfLink = this.text.toString().indexOf(link.first)
+            spannableString.setSpan(clickableSpan, startIndexOfLink, startIndexOfLink + link.first.length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+        this.movementMethod = LinkMovementMethod.getInstance() // without LinkMovementMethod, link can not click
+        this.setText(spannableString, TextView.BufferType.SPANNABLE)
+    }
+}
